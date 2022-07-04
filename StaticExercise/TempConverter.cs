964199796501d30731c4f6a880a9eb39c7742ccc
Brooks Adams <aba1421@jagmail.southalabama.edu>
2022-07-03 23:33:33 -0500

    init commit

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            var celTemp = (fTemp - 32) * (5.0 / 9.0);
+            return celTemp;
+        }
+
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            return (cTemp * (9.0 / 5.0)) + 32;
+        }
+    }
+}
